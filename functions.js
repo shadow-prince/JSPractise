6604ec1d0b89e902910f2240dfd81f1ed2b51464
Sai Kishore <59025976+shadow-prince@users.noreply.github.com>
2020-09-21 15:05:34 +0530

    added table reset

--- functions.js
+++ functions.js
@@ -12,9 +12,13 @@ function multiplicationTable()
 {
     a = Number(document.getElementById("mulA").value);
     let new1;
+    newl=document.getElementById("product");
+    newl.innerHTML =" ";
     for(let i =1; i<=10;i++)
-    {
-        newl=document.getElementById("product")
+    {if(i==1)
+        {
+           newl.innerHTML+="<tr>" + "<h4>Table of "+a +"</h4></tr>";
+        }
         newl.innerHTML += "<tr><td>"+a+"x" + i + "=" + a*i +"</td></tr>"; 
     }
 }
